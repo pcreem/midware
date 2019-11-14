e7cd934f50277715ecf3e3e623dcda9b9d095924
pcreem <47106025+pcreem@users.noreply.github.com>
2019-11-14 13:11:13 +0800

    Update response-time-logger.js

--- response-time-logger.js
+++ response-time-logger.js
@@ -1,12 +1,13 @@
 // response-time-logger.js
 function logResponseTime(req, res, next) {
+  var start = Date()
   const startHrTime = process.hrtime();
 
   res.on("finish", () => {
     const elapsedHrTime = process.hrtime(startHrTime);
     const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
     var fullUrl = req.method + ' from ' + req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(Date(), " | ", fullUrl, " | total time", elapsedTimeInMs, "ms");
+    console.log(start, " | ", fullUrl, " | total time", elapsedTimeInMs, "ms");
   });
 
   next();
